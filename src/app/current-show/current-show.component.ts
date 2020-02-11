bd0ab7a6bb3619993786b08840804e8b3f9b0ece
DaraiSidorko <daria.sidorko@gmail.com>
2020-02-11 00:24:33 -0800

    1st component built with dummy data

--- src/app/current-show/current-show.component.ts
+++ src/app/current-show/current-show.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ICurrentShow } from '../icurrent-show';
 
 @Component({
   selector: 'app-current-show',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-show.component.css']
 })
 export class CurrentShowComponent implements OnInit {
-
-  constructor() { }
+  current: ICurrentShow;
+  constructor() {
+    this.current = {
+      name:'Girls',
+      language: 'English',
+      genres: 'Drama, Romance',
+      officialSite: 'http://www.hbo.com/girls',
+      runtime: 30,
+      showTime: 'Sunday 22:00',
+      raiting: 6.9,
+      network: 'HBO', //network -> name
+      country: 'US', 
+      image: 'http://static.tvmaze.com/uploads/images/medium_portrait/31/78286.jpg',
+      summary: 'This Emmy winning series is a comic look at the assorted humiliations and rare triumphs of a group of girls in their 20s.',
+    } as ICurrentShow
+   }
 
   ngOnInit(): void {
   }
--- src/app/icurrent-show.ts
+++ src/app/icurrent-show.ts
@@ -1,2 +1,14 @@
 export interface ICurrentShow {
+  name: string,
+  language: string,
+  genres: string,
+  officialSite: string,
+  runtime: number,
+  showTime: string,
+  raiting: number,
+  network: string, //network -> name
+  country: string, //country->code
+  image: string,
+  summary: string,
 }
+
