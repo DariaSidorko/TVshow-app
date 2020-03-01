1746ef585f571920d2931f4dc352ed98d88e050a
surryjm <50641921+surryjm@users.noreply.github.com>
2020-02-29 22:00:09 -0800

    Null check on cast service, spec testing, media qs

--- src/app/front-page/front-page.component.spec.ts
+++ src/app/front-page/front-page.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FrontPageComponent } from './front-page.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('FrontPageComponent', () => {
   let component: FrontPageComponent;
@@ -8,6 +9,7 @@ describe('FrontPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ FrontPageComponent ]
     })
     .compileComponents();
--- src/app/show-search/show-search.component.spec.ts
+++ src/app/show-search/show-search.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShowSearchComponent } from './show-search.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ShowSearchComponent', () => {
   let component: ShowSearchComponent;
@@ -8,6 +9,7 @@ describe('ShowSearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ ShowSearchComponent ]
     })
     .compileComponents();
